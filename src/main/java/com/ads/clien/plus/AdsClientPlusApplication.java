17176f71e9e414b387af9bb3ee28985efacef16c
Adson Conceição <adsonconcecao@yahoo.com.br>
2025-03-08 15:14:42 -0300

    Criando projeto e iniciando db.migration V1__create

---
+++ src/main/java/com/ads/clien/plus/AdsClientPlusApplication.java
@@ -0,0 +1,13 @@
+package com.ads.clien.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsClientPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsClientPlusApplication.class, args);
+	}
+
+}
